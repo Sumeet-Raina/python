4bf83607280e773f772db749c7c4537c230bcc97
sumeet978 <searchsumeet@gmail.com>
2020-01-13 19:53:07 +0000

    String datatype

---
+++ string_datatypes.py
@@ -0,0 +1,12 @@
+# strings
+# escape sequence
+dessert = "Marshmallows"
+print(dessert)
+dessert = dessert + " and chocolate"
+print(dessert)
+dessert += " and graham crackers"
+print(dessert)
+print('I can\'t  do this tomorrow')
+print('I can \n do this tomorrow')
+print ("Hello" + " World")
+print("""She said, "I can't even."  """)
