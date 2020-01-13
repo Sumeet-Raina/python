6c72c0072e4da38275c877e8d5ade1859e23e0dd
sumeet978 <searchsumeet@gmail.com>
2020-01-13 20:33:16 +0000

    using booleans

---
+++ boolean.py
@@ -0,0 +1,17 @@
+has_taco = "taco" in "catacombs"
+print(has_taco) #True
+has_taco = "taco" in "combucha"
+print(has_taco) #False
+#bool(1) // True
+#bool(0) // False
+#bool(32) // True
+#bool("Burrito") // True
+#bool("") // False
+#True and True // True
+#True and false // False
+#True or True // True
+#True or false // True
+#False and false // False
+#False and  not false // False
+
+
