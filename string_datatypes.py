76fcacf89e5dcbe3180347f51b39e6e2e97abe78
sumeet978 <searchsumeet@gmail.com>
2020-01-13 20:09:44 +0000

    string methods

--- string_datatypes.py
+++ string_datatypes.py
@@ -6,6 +6,12 @@ dessert = dessert + " and chocolate"
 print(dessert)
 dessert += " and graham crackers"
 print(dessert)
+quote = "Consistency and Persistance is everything"
+print(len(quote))
+print(quote.upper())
+print(quote.lower())
+print(quote.title())
+print(quote.capitalize())
 print('I can\'t  do this tomorrow')
 print('I can \n do this tomorrow')
 print ("Hello" + " World")
