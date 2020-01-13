85f703d69bb47278830089e3aaed604cad9ad98e
sumeet978 <searchsumeet@gmail.com>
2020-01-13 20:17:32 +0000

    using string format

---
+++ string_formatting.py
@@ -0,0 +1,4 @@
+subject_template = "I would love to visit {} in {}!"
+print(subject_template.format("Dubai", 2020))
+print(subject_template.format("Austria", 2020))
+print("I would love to visit {} in {}!".format("France", 2020))
